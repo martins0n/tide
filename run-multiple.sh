45c30e2ca2e25379217539d0166242cbf0704bf5
martins0n <33594071+martins0n@users.noreply.github.com>
2023-07-11 22:58:47 +0300

    fix:  add cuda

--- run-multiple.sh
+++ run-multiple.sh
@@ -1,9 +1,9 @@
 horizon=${horizon:-96}
 
-python pipe_raw.py  +experiment=etth2 experiment.horizon=$horizon +dataset=etth2 +model=etth2 model.max_epochs=50
-python pipe_raw.py  +experiment=etth1 experiment.horizon=$horizon +dataset=etth1 +model=etth1 model.max_epochs=50
-python pipe_raw.py +experiment=ettm1 experiment.horizon=$horizon +dataset=ettm1 +model=ettm1 model.max_epochs=50
-python pipe_raw.py +experiment=ettm2 experiment.horizon=$horizon +dataset=ettm2 +model=ettm2 model.max_epochs=50
-python pipe_raw.py +experiment=electricity_96 experiment.horizon=$horizon +dataset=electricity +model=electricity_96 model.max_epochs=20
-python pipe_raw.py +experiment=traffic experiment.horizon=$horizon +dataset=traffic +model=traffic model.max_epochs=20
-python pipe_raw.py +experiment=weather experiment.horizon=$horizon +dataset=weather +model=weather model.max_epochs=20
+python pipe_raw.py  +experiment=etth2 experiment.horizon=$horizon +dataset=etth2 +model=etth2 model.max_epochs=50 accelerator=cuda
+python pipe_raw.py  +experiment=etth1 experiment.horizon=$horizon +dataset=etth1 +model=etth1 model.max_epochs=50 accelerator=cuda
+python pipe_raw.py +experiment=ettm1 experiment.horizon=$horizon +dataset=ettm1 +model=ettm1 model.max_epochs=50 accelerator=cuda
+python pipe_raw.py +experiment=ettm2 experiment.horizon=$horizon +dataset=ettm2 +model=ettm2 model.max_epochs=50 accelerator=cuda
+python pipe_raw.py +experiment=electricity_96 experiment.horizon=$horizon +dataset=electricity +model=electricity_96 model.max_epochs=5 accelerator=cuda
+python pipe_raw.py +experiment=traffic experiment.horizon=$horizon +dataset=traffic +model=traffic model.max_epochs=5 accelerator=cuda
+python pipe_raw.py +experiment=weather experiment.horizon=$horizon +dataset=weather +model=weather model.max_epochs=5 accelerator=cuda
